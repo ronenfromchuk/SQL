5cdc6d2b604fd6f3c9590ca150e23ffdab771826
Ronen Fromchuk <90146636+ronenfromchuk@users.noreply.github.com>
2021-12-26 20:51:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -64,4 +64,37 @@ repo.add(m2)
 m3 = MixOrdersProducts(order_id = 2, product_id = 3, quantity = 5)
 repo.add(m3)
 
+o_m = repo.get_by_condition(MixOrdersProducts,  lambda query: query.filter(MixOrdersProducts.order_id == 1).all())
+print('result ===', o_m)
+print('products =========', o_m[0].product)
 
+o = repo.get_by_condition(Order,  lambda query: query.filter(Order.id == 1).first())
+print('result ===', o)
+print('products from order id 1=========\n', o.mix_orders_products)
+
+p = repo.get_by_condition(Product,  lambda query: query.filter(Product.id == 2).first())
+print('result ===', p)
+print('orders for product id 2=========\n', p.mix_orders_products)
+
+repo.add(Student(name='danny'))
+repo.add(Student(name='moshe'))
+
+repo.add(Teacher(name='suzi'))
+repo.add(Teacher(name='tzipi'))
+
+repo.add(Subject(name='python'))
+repo.add(Subject(name='english'))
+repo.add(Subject(name='math'))
+
+repo.add(Lessons(subject_id=2, teacher_id=2, student_id=1))
+repo.add(Lessons(subject_id=3, teacher_id=1, student_id=1))
+repo.add(Lessons(subject_id=1, teacher_id=2, student_id=2))
+
+danny = repo.get_by_condition(Student,  lambda query: query.filter(Student.id == 1).first())
+print(danny.lessons[0].teacher.name)
+
+tzipi = repo.get_by_condition(Teacher,  lambda query: query.filter(Teacher.id == 2).first())
+print(tzipi.lessons)
+
+lessons_with_tzipi = repo.get_by_condition(Lessons,  lambda query: query.filter(Lessons.teacher_id == 2).all())
+print('lessons_with_tzipi =========== ', lessons_with_tzipi)
